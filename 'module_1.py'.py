f4cf728a339ade4d667da72b22224793094bbe73
Gulya1098 <mansurova221098@gmail.com>
2024-12-05 16:51:33 +0500

    Add files via upload

---
+++ 'module_1.py'.py
@@ -0,0 +1,6 @@
+completed_homework = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = hours_spent / completed_homework
+print(time_for_one_task)
+print('Курс:',course_name, 'всего задач:', completed_homework, 'затрачено часов:', hours_spent, 'среднее время выпонения', time_for_one_task )
